77e4cdeabbecc987d0bc776a350ffec92cf3bcd6
Housey <housebreaker1@gmail.com>
2015-12-04 18:55:35 +0200

    add 5th Homework with first problem

---
+++ 05. Other Types in OOP/01. Galactic GPS/GalacticGPSTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.Galactic_GPS
+{
+	static class GalacticGPSTest
+	{
+		static void Main()
+		{
+			var home = new Location(18.037986, 28.870097, Planet.Earth);
+			Console.WriteLine(home);
+		}
+	}
+}
---
+++ 05. Other Types in OOP/01. Galactic GPS/Location.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _01.Galactic_GPS
+{
+	public struct Location
+	{
+		private const int LatitudeLimit = 90;
+		private const int LongitudeLimit = 180;
+
+		private double latitude;
+		private double longitude;
+		private Planet planet;
+
+		public Location(double latitude, double longitude, Planet planet) : this()
+		{
+			this.Latitude = latitude;
+			this.Longitude = longitude;
+			this.Planet = planet;
+		}
+
+		public double Latitude
+		{
+			get { return latitude; }
+			set
+			{
+				if (value < -LatitudeLimit || value > LatitudeLimit)
+				{
+					throw new ArgumentOutOfRangeException(nameof(latitude), "Latitude needs to be in range [-90..90]");
+				}
+
+				latitude = value;
+			}
+		}
+
+		public double Longitude
+		{
+			get { return longitude; }
+			set
+			{
+				if (value < -LongitudeLimit || value > LongitudeLimit)
+				{
+					throw new ArgumentOutOfRangeException(nameof(longitude), "Longitude needs to be in range [-180..180]");
+				}
+				
+				longitude = value;
+			}
+		}
+
+		public Planet Planet
+		{
+			get { return planet; }
+			set { planet = value; }
+		}
+
+		public override string ToString()
+		{
+			return $"{Latitude}, {Longitude} - {Planet}";
+		}
+	}
+}
