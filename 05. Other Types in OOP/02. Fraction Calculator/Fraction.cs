d82077671d9382f675018ef8a1a49268ffd9ab0b
Housey <housebreaker1@gmail.com>
2015-12-04 19:39:58 +0200

    add 2nd problem

---
+++ 05. Other Types in OOP/02. Fraction Calculator/Fraction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace _02.Fraction_Calculator
+{
+	public struct Fraction
+	{
+		private long numerator;
+		private long denominator;
+
+		public Fraction(long numerator, long denominator) : this()
+		{
+			this.Numerator = numerator;
+			this.Denominator = denominator;
+		}
+
+		public long Numerator
+		{
+			get { return numerator; }
+			set { numerator = value; }
+		}
+
+		public long Denominator
+		{
+			get { return denominator; }
+			set
+			{
+				if (value == 0)
+				{
+					throw new ArgumentOutOfRangeException(nameof(Denominator), "The denominator cannot be 0.");
+				}
+				denominator = value;
+			}
+		}
+
+		public static Fraction operator +(Fraction f1, Fraction f2)
+		{
+			var largestCommonDenominator = f1.Denominator*f2.Denominator;
+
+			var numerator1 = f1.Numerator*f2.Denominator;
+			var numerator2 = f2.Numerator*f1.Denominator;
+
+			return new Fraction(numerator1 + numerator2, largestCommonDenominator);
+		}
+
+		public override string ToString()
+		{
+			return ((decimal)Numerator/Denominator).ToString(CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ 05. Other Types in OOP/02. Fraction Calculator/FractionCalcMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.Fraction_Calculator
+{
+	class FractionCalcMain
+	{
+		static void Main()
+		{
+			var fraction1 = new Fraction(22, 7);
+			var fraction2 = new Fraction(40, 4);
+
+			var result = fraction1 + fraction2;
+
+			Console.WriteLine(result.Numerator);
+			Console.WriteLine(result.Denominator);
+			Console.WriteLine(result);
+		}
+	}
+}
